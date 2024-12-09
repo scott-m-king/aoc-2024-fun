0d645fc1b62dcbad0366ee0ba3bbc5ad95727dab
Scott <scott.m.king@outlook.com>
2024-12-08 19:22:08 -0800

    day2 pt 2 ocaml

--- aoc_2024_ocaml/lib/solutions/day2.ml
+++ aoc_2024_ocaml/lib/solutions/day2.ml
@@ -5,9 +5,9 @@ let parse_input str =
          |> List.filter_map (fun y ->
                 try Some (int_of_string y) with Failure _ -> None))
 
-let rec is_sorted func = function
+let rec is_sorted cmp = function
   | [] | [ _ ] -> true
-  | x :: y :: xs -> func x y && is_sorted func (y :: xs)
+  | x :: y :: xs -> cmp x y && is_sorted cmp (y :: xs)
 
 let is_safe list =
   list |> Utils.windows 2
@@ -18,11 +18,28 @@ let is_safe list =
              1 <= diff && 3 >= diff
          | _ -> false)
 
-let part1 =
-  Utils.read_file "data/day-2.txt"
-  |> parse_input
+let part1 input =
+  input |> parse_input
   |> List.filter (fun lst ->
          (is_sorted ( > ) lst || is_sorted ( < ) lst) && is_safe lst)
   |> List.length
 
-let get_solution () = part1 |> print_int
+let part2 input =
+  input |> parse_input
+  |> List.filter (fun line ->
+         let len = List.length line in
+         let rec try_removing_at i =
+           if i >= len then false
+           else
+             let filtered = List.filteri (fun j _ -> i <> j) line in
+             if
+               (is_sorted ( > ) filtered || is_sorted ( < ) filtered)
+               && is_safe filtered
+             then true
+             else try_removing_at (i + 1)
+         in
+         try_removing_at 0)
+  |> List.length
+
+let input = Utils.read_file "data/day-2.txt"
+let get_solution () = part2 input |> print_int
