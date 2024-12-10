327e5a826ac9ac24b1ee1cde0165fee91fd91505
Scott <scott.m.king@outlook.com>
2024-12-10 12:28:21 -0800

    Day 4 pt 2

--- aoc_2024_ocaml/lib/solutions/day4.ml
+++ aoc_2024_ocaml/lib/solutions/day4.ml
@@ -1,4 +1,10 @@
-type cell = { letter : char; x : int; y : int }
+type cell = { l : char; x : int; y : int }
+
+let print_cell_list_vertical cells =
+  print_endline "Corners:";
+  List.iteri
+    (fun i cell -> Printf.printf "  %d: { letter: '%c'; x: %d; y: %d }\n" i cell.l cell.x cell.y)
+    cells
 
 let make_indices w h = List.init h (fun y -> List.init w (fun x -> (x, y))) |> List.flatten
 
@@ -10,11 +16,11 @@ let parse_input input =
 let get_cell grid row col (i, j) =
   let x = row + i in
   let y = col + j in
-  try Some { letter = grid.(x).(y); x; y } with
+  try Some { l = grid.(x).(y); x; y } with
   | Invalid_argument _ -> None
 
 let rec find_xmas grid cell direction =
-  match cell.letter with
+  match cell.l with
   | 'X' -> find_next grid 'M' cell direction
   | 'M' -> find_next grid 'A' cell direction
   | 'A' -> find_next grid 'S' cell direction
@@ -23,7 +29,7 @@ let rec find_xmas grid cell direction =
 
 and find_next grid next_letter cell direction =
   match get_cell grid cell.x cell.y direction with
-  | Some next_cell when next_cell.letter = next_letter -> find_xmas grid next_cell direction
+  | Some next_cell when next_cell.l = next_letter -> find_xmas grid next_cell direction
   | _ -> 0
 
 let part1 input =
@@ -36,10 +42,33 @@ let part1 input =
          | 'X' ->
            acc
            + ([ (0, 1); (0, -1); (1, 0); (-1, 0); (1, 1); (-1, -1); (1, -1); (-1, 1) ]
-             |> List.map (fun dir -> find_xmas grid { letter = 'X'; x; y } dir)
+             |> List.map (fun dir -> find_xmas grid { l = 'X'; x; y } dir)
              |> List.fold_left ( + ) 0)
          | _ -> acc)
        0
 
-let part2 _input = 0
-let get_solution () = part1 (Utils.read_file "data/day-4.txt") |> print_int
+let get_corners grid x y =
+  [ (-1, -1); (1, -1); (-1, 1); (1, 1) ] (* nw sw ne se *)
+  |> List.filter_map (fun dir -> get_cell grid x y dir)
+  |> List.filter (fun x -> x.l = 'M' || x.l = 'S')
+
+let part2 input =
+  let grid = parse_input input in
+  make_indices (Array.length grid) (Array.length grid.(0))
+  |> List.fold_left
+       (fun acc (x, y) ->
+         match grid.(x).(y) with
+         | 'A' ->
+           let corners = get_corners grid x y in
+           if List.length corners = 4 then
+             acc
+             +
+             match corners with
+             | [ nw; sw; ne; se ] when nw.l <> se.l && ne.l <> sw.l -> 1
+             | _ -> 0
+           else
+             acc
+         | _ -> acc)
+       0
+
+let get_solution () = part2 (Utils.read_file "data/day-4.txt") |> print_int
