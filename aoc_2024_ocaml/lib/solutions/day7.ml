9bf60add0e049237a4789c0a4be5a784a45e80d7
Scott <scott.m.king@outlook.com>
2024-12-15 16:43:06 -0800

    Be tricky!

--- aoc_2024_ocaml/lib/solutions/day7.ml
+++ aoc_2024_ocaml/lib/solutions/day7.ml
@@ -9,24 +9,28 @@ let parse_input str =
          (int_of_string target, value)
        | _ -> (0, []))
 
-let rec try_combos ?(acc = 0) (target, nums) : bool =
-  let next =
-    match nums with
-    | x :: xs -> Some (x, xs)
-    | _ -> None
-  in
+let get_next_nums = function
+  | x :: xs -> Some (x, xs)
+  | _ -> None
 
-  match (acc, next) with
+let rec try_combos acc (target, nums) ops : bool =
+  match (acc, get_next_nums nums) with
+  | x, _ when x > target -> false
   | x, None when x = target -> true
   | x, Some (next_num, rest_nums) ->
-    let add = try_combos ~acc:(x + next_num) (target, rest_nums) in
-    let multiply = try_combos ~acc:(x * next_num) (target, rest_nums) in
-    add || multiply
+    List.exists (fun fn -> try_combos (fn x next_num) (target, rest_nums) ops) ops
   | _ -> false
 
 let part1 input =
-  parse_input input |> List.filter try_combos
+  let ops = [ ( + ); ( * ) ] in
+  parse_input input
+  |> List.filter (fun x -> try_combos 0 x ops)
   |> List.fold_left (fun acc (target, _) -> acc + target) 0
 
-let part2 _input = 0
-let get_solution () = part1 (read_file "data/day-7.txt") |> print_int
+let part2 input =
+  let ops = [ ( + ); ( * ); (fun x y -> Printf.sprintf "%d%d" x y |> int_of_string) ] in
+  parse_input input
+  |> List.filter (fun x -> try_combos 0 x ops)
+  |> List.fold_left (fun acc (target, _) -> acc + target) 0
+
+let get_solution () = part2 (read_file "data/day-7.txt") |> print_int
