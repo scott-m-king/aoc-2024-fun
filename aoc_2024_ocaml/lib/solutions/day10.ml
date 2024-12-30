ac96044c7b47255852ef282d0f6fbdf9c2f13e6f
Scott <scott.m.king@outlook.com>
2024-12-29 16:54:09 -0800

    Use generic dfs

--- aoc_2024_ocaml/lib/solutions/day10.ml
+++ aoc_2024_ocaml/lib/solutions/day10.ml
@@ -15,7 +15,7 @@ let parse_input str =
   |> Array.map (fun row -> row |> Array.map (fun x -> int_of_char x - int_of_char '0'))
   |> fun grid -> grid, make_indices grid
 
-let rec dfs grid curr_cell visited =
+let rec dfs grid curr_cell (func : int_cell -> 'a -> 'a) (start : 'a) : 'a =
   directions
   |> List.fold_left
        (fun acc (row, col) ->
@@ -24,10 +24,10 @@ let rec dfs grid curr_cell visited =
          | Some next_cell when next_cell.value = curr_cell.value + 1 ->
            next_cell.value
            |> (function
-            | 9 -> VisitedSet.add next_cell acc
-            | _ -> dfs grid next_cell acc)
+            | 9 -> func next_cell acc
+            | _ -> dfs grid next_cell func acc)
          | _ -> acc)
-       visited
+       start
 
 let part1 input =
   parse_input input
@@ -37,10 +37,21 @@ let part1 input =
        (fun acc x ->
          match get_int_cell grid x with
          | Some trailhead when trailhead.value = 0 ->
-           acc + VisitedSet.cardinal (dfs grid trailhead VisitedSet.empty)
+           let func next_cell visited = VisitedSet.add next_cell visited in
+           acc + VisitedSet.cardinal (dfs grid trailhead func VisitedSet.empty)
          | _ -> acc)
        0
 
-let part2 _input = 0
+let part2 input =
+  parse_input input
+  |> fun (grid, indices) ->
+  indices
+  |> List.fold_left
+       (fun acc x ->
+         match get_int_cell grid x with
+         | Some trailhead when trailhead.value = 0 ->
+           acc + dfs grid trailhead (fun _ acc -> acc + 1) 0
+         | _ -> acc)
+       0
 
-let get_solution () = part1 (read_file "data/day-10.txt") |> print_int
+let get_solution () = part2 (read_file "data/day-10.txt") |> print_int
